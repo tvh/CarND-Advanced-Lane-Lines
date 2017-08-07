cd3ce5e98c09b43d906e0e5db30a87b2fe8f1576
Timo von Holtz <tvh@tvholtz.de>
2017-08-07 19:27:54 +0200

    Combine thresholds meaningfully

--- pipeline.py
+++ pipeline.py
@@ -20,12 +20,13 @@ def warp(img, src=np.float32([[596,450],[688,450],[1048,681],[276,681]])):
     return warped, M
 
 def thresh_r(img):
-    """Apply a binary thresholds to the R channel"""
+    """Apply a binary threshold to the R channel"""
     b, g, r = cv2.split(img)
     binary_r = cv2.adaptiveThreshold(r, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 255, -70)
-    return binary_r
+    return cleanup_threshold(binary_r)
 
 def thresh_sobel(img):
+    """Apply a binary threshold to the sobel operator"""
     b, g, r = cv2.split(img)
     sobelx = cv2.Sobel(cv2.blur(r, (25,17)), cv2.CV_64F, 1, 0)
     min_sobel = np.min(sobelx)
@@ -34,7 +35,28 @@ def thresh_sobel(img):
     scaled_sobel_r = 255-scaled_sobel_l
     sobel_binary_l = cv2.adaptiveThreshold(scaled_sobel_l, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 255, -30)
     sobel_binary_r = cv2.adaptiveThreshold(scaled_sobel_r, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 255, -30)
-    return sobel_binary_l, sobel_binary_r
+    return cleanup_threshold(sobel_binary_l), cleanup_threshold(sobel_binary_r)
+
+def cleanup_threshold(binary):
+    """Cleans up a given thresholded channel. This assumes sharp lane markings"""
+    height, width = binary.shape[:2]
+    trans_m_l = np.float32([[1, 0, 12], [0, 1, 0]])
+    trans_m_r = np.float32([[1, 0, -12], [0, 1, 0]])
+    shifted_binary_l = cv2.warpAffine(binary, trans_m_l, (width, height))
+    shifted_binary_r = cv2.warpAffine(binary, trans_m_r, (width, height))
+    return binary-shifted_binary_l-shifted_binary_r
+
+def combine_thresholds(binary_r, binary_sobel_l, binary_sobel_r):
+    """Combine the calculated thresholds"""
+    height, width = binary_r.shape[:2]
+    trans_m_l = np.float32([[1, 0, 12], [0, 1, 0]])
+    shifted_sobel_l = cv2.warpAffine(binary_sobel_l, trans_m_l, (width, height))
+    trans_m_r = np.float32([[1, 0, -12], [0, 1, 0]])
+    shifted_sobel_r = cv2.warpAffine(binary_sobel_r, trans_m_r, (width, height))
+    combined = (shifted_sobel_l//3+shifted_sobel_r//3+binary_r//3)
+    res = np.zeros_like(combined)
+    res[combined>150]=255
+    return res
 
 def load_calibration_params():
     cal_file = open('calibration.pkl','rb')
@@ -57,5 +79,7 @@ def output_test_images():
         binary_sobel_l, binary_sobel_r = thresh_sobel(warped)
         thresholded = cv2.merge([binary_sobel_l, binary_r, binary_sobel_r])
         cv2.imwrite('output_images/thresholded_'+os.path.basename(fname), thresholded)
+        combined = combine_thresholds(binary_r, binary_sobel_l, binary_sobel_r)
+        cv2.imwrite('output_images/combined_'+os.path.basename(fname), combined)
 
 output_test_images()
