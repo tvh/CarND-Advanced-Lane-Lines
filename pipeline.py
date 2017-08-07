d141187b1d610e04a5a475ed9d9c2f25c63d9197
Timo von Holtz <tvh@tvholtz.de>
2017-08-07 18:17:11 +0200

    Thresholding on sobel operator as well

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,7 @@ import glob
 import pickle
 import os
 
-def warp(img, src=np.float32([[574,465],[711,465],[1048,681],[276,681]])):
+def warp(img, src=np.float32([[596,450],[688,450],[1048,681],[276,681]])):
     """Warp the image for top-down view"""
     img_size = (256,512)
     x1 = img_size[0]/4
@@ -19,11 +19,20 @@ def warp(img, src=np.float32([[574,465],[711,465],[1048,681],[276,681]])):
 
     return warped, M
 
-def threshold(img):
-    """Apply a binary threshold"""
+def thresh_r(img):
+    """Apply a binary threshold to the R channel"""
     b, g, r = cv2.split(img)
-    binary = cv2.adaptiveThreshold(r, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 255, -75)
+    binary = cv2.adaptiveThreshold(r, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 255, -70)
     return binary
+
+def thresh_sobel(img):
+    """Apply a binary threshold on the sobel operator"""
+    b, g, r = cv2.split(img)
+    sobelx = cv2.Sobel(cv2.blur(r, (25,17)), cv2.CV_64F, 1, 0)
+    abs_sobelx = np.absolute(sobelx)
+    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+    sobel_binary = cv2.adaptiveThreshold(scaled_sobel, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 255, -40)
+    return sobel_binary
 
 def load_calibration_params():
     cal_file = open('calibration.pkl','rb')
@@ -42,7 +51,9 @@ def output_test_images():
         cv2.imwrite('output_images/undistorted_'+os.path.basename(fname), undist)
         warped, M = warp(undist)
         cv2.imwrite('output_images/warped_'+os.path.basename(fname), warped)
-        binary = threshold(warped)
-        cv2.imwrite('output_images/binary_'+os.path.basename(fname), binary)
+        binary_r = thresh_r(warped)
+        binary_sobel = thresh_sobel(warped)
+        thresholded = cv2.merge([np.zeros_like(binary_r), binary_r, binary_sobel])
+        cv2.imwrite('output_images/thresholded_'+os.path.basename(fname), thresholded)
 
 output_test_images()
