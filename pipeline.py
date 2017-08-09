8acddc8a457f5d0007c6d07f689854a3e0bed021
Timo von Holtz <tvh@tvholtz.de>
2017-08-09 18:12:15 +0200

    Implement searching using previous fit
    This doesn't seem to help much though

--- pipeline.py
+++ pipeline.py
@@ -76,6 +76,8 @@ def fit_lane_line(
         minpix=20,
         visualize=False,
         min_y_dist=100,
+        prev_left_fit=None,
+        prev_right_fit=None
 ):
     """
     Finds the lane lines and fits a curve to it.
@@ -98,9 +100,6 @@ def fit_lane_line(
     nonzero = binary_warped.nonzero()
     nonzeroy = np.array(nonzero[0])
     nonzerox = np.array(nonzero[1])
-    # Current positions to be updated for each window
-    leftx_current = leftx_base
-    rightx_current = rightx_base
     # Create empty lists to receive left and right lane pixel indices
     left_lane_inds = []
     right_lane_inds = []
@@ -109,35 +108,37 @@ def fit_lane_line(
         out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
     else:
         out_img = None
-    # Step through the windows one by one
-    for window in range(nwindows):
-        # Identify window boundaries in x and y (and right and left)
-        win_y_low = binary_warped.shape[0] - (window+1)*window_height
-        win_y_high = binary_warped.shape[0] - window*window_height
-        win_xleft_low = leftx_current - margin
-        win_xleft_high = leftx_current + margin
-        win_xright_low = rightx_current - margin
-        win_xright_high = rightx_current + margin
 
-        if visualize:
-            # Draw the windows on the visualization image
-            cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2)
-            cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2)
-        # Identify the nonzero pixels in x and y within the window
-        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
-        # Append these indices to the lists
-        left_lane_inds.append(good_left_inds)
-        right_lane_inds.append(good_right_inds)
-        # If you found > minpix pixels, recenter next window on their mean position
-        if len(good_left_inds) > minpix:
-            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-        if len(good_right_inds) > minpix:
-            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+    def windowed_search(x_current):
+        lane_inds = []
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = binary_warped.shape[0] - (window+1)*window_height
+            win_y_high = binary_warped.shape[0] - window*window_height
+            win_x_low = x_current - margin
+            win_x_high = x_current + margin
 
-    # Concatenate the arrays of indices
-    left_lane_inds = np.concatenate(left_lane_inds)
-    right_lane_inds = np.concatenate(right_lane_inds)
+            if visualize:
+                # Draw the window on the visualization image
+                cv2.rectangle(out_img,(win_x_low,win_y_low),(win_x_high,win_y_high),(0,255,0), 2)
+            # Identify the nonzero pixels in x and y within the window
+            good_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_x_low) & (nonzerox < win_x_high)).nonzero()[0]
+            # Append these indices to the lists
+            lane_inds.append(good_inds)
+            # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_inds) > minpix:
+                x_current = np.int(np.mean(nonzerox[good_inds]))
+        return np.concatenate(lane_inds)
+
+    if prev_left_fit!=None:
+        left_lane_inds = ((nonzerox > (prev_left_fit[0]*(nonzeroy**2) + prev_left_fit[1]*nonzeroy + prev_left_fit[2] - margin)) & (nonzerox < (prev_left_fit[0]*(nonzeroy**2) + prev_left_fit[1]*nonzeroy + prev_left_fit[2] + margin)))
+    else:
+        left_lane_inds = windowed_search(leftx_base)
+    if prev_right_fit!=None:
+        right_lane_inds = ((nonzerox > (prev_right_fit[0]*(nonzeroy**2) + prev_right_fit[1]*nonzeroy + prev_right_fit[2] - margin)) & (nonzerox < (prev_right_fit[0]*(nonzeroy**2) + prev_right_fit[1]*nonzeroy + prev_right_fit[2] + margin)))
+    else:
+        right_lane_inds = windowed_search(rightx_base)
 
     # Extract left and right line pixel positions
     leftx = nonzerox[left_lane_inds]
@@ -257,9 +258,13 @@ def annotate_video(src, dst):
     # TODO: Lookup if Currying is possible in python.
     prev_left_fit = None
     prev_right_fit = None
+    left_current = False
+    right_current = False
     def process_image(img):
         nonlocal prev_left_fit
         nonlocal prev_right_fit
+        nonlocal left_current
+        nonlocal right_current
         # The pipeline works on BGR images
         color_corrected = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         undist = cv2.undistort(color_corrected, mtx, dist, None, mtx)
@@ -268,7 +273,15 @@ def annotate_video(src, dst):
         binary_sobel_l, binary_sobel_r = thresh_sobel(warped)
         thresholded = cv2.merge([binary_sobel_l, binary_r, binary_sobel_r])
         combined = combine_thresholds(binary_r, binary_sobel_l, binary_sobel_r)
-        left_fit, right_fit, _ = fit_lane_line(combined)
+        if left_current:
+            prev_left_arg = prev_left_fit
+        else:
+            prev_left_arg = None
+        if right_current:
+            prev_right_arg = prev_right_fit
+        else:
+            prev_right_arg = None
+        left_fit, right_fit, _ = fit_lane_line(combined, prev_left_fit=prev_left_arg, prev_right_fit=prev_right_arg)
         # Make sure we have a mapping if we lose track for a short time
         if left_fit==None:
             left_fit = prev_left_fit
